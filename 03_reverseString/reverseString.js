2a7d71f3653dc0037c4e4492f3767feedb894026
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-16 14:29:38 -0500

    fix exercises 01 - 04 to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = [];
+    for (i = 0; i < string.length; i++) {
+        array.unshift(string[i]);
+    }
+    let newString = array.join('');
+    return newString;
 };
 
 // Do not edit below this line
