dd320ae40851717879240d4d52690247341a8b27
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-15 22:09:56 -0500

    pass first test by removing single value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removed) {
+    let i = 0;
+    while (i < array.length) {
+        if (array[i] == removed){
+            array.splice(i, 1);
+            return array;
+        }
+        i++
+    }
 };
 
 // Do not edit below this line
