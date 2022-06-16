277afe64e622cb5fc75ae47af367bc6996da54d4
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-16 14:26:23 -0500

    fix to pass all tests. change to 'for' loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,16 @@
-const removeFromArray = function(array, ...removed) {
-    let i = 0;
-    while (i < array.length) {
-        if (array[i] == removed){
-            array.splice(i, 1);
-            return array;
+const removeFromArray = function(array, ...removeArguments) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0, argumentArray = Array.from(removeArguments); j < argumentArray.length; j++) {
+            if (array[i] === argumentArray[j]) {
+                delete array[i];
+            }
         }
-        i++
     }
-};
+//removes undefined elements
+    let filteredArray = array.filter(element => element !== undefined);
+    return filteredArray;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
