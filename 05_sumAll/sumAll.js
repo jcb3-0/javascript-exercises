c73026f5b3314ea519f4233f6a81fb065c495e6e
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-16 22:31:29 -0500

    fix to pass first 5 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    let sum = 0;
+    if (typeof begin === 'string' || typeof end === 'string') {
+        return "ERROR";
+    } if (begin < 0 || end < 0) {
+        return "ERROR";
+    } else if (begin < end) {
+        for (let i = begin; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (begin > end) {
+        for (let i = end; i <= begin; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
