cd1ba6affaa3d897e0cb69e75edd5c77036666ec
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-16 23:17:17 -0500

    fix to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if (temp == 32) {
+    return 0;
+  }
+  return parseFloat(((temp - 32) * (5/9)).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if (temp == 0) {
+    return 32;
+  }
+  return parseFloat(((temp * (9/5)) + 32).toFixed(1))
 };
 
 // Do not edit below this line
