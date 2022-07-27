ab198f17e09fa9e9702d0c06b9395550130e68e7
Jason Bauzon <jasonbauzon@gmail.com>
2022-07-26 22:51:06 -0500

    works without special characters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let uppercase = string.toUpperCase();
+    let stringArray = Array.from(uppercase);
+    let backwardsArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        backwardsArray.unshift(stringArray[i]);
+    };
+    for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i] != backwardsArray[i]) {
+            console.log(stringArray[i]);
+            return false;
+        } else if (stringArray[i] == backwardsArray[i]){
+            console.log(stringArray[i]);
+            if (i == stringArray.length - 1) {
+                return true;
+            };
+            continue;
+        };
+    };
 };
 
 // Do not edit below this line
