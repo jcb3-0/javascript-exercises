4434e5c69cf6bf4811fddbb16392b0285f137bf9
Jason Bauzon <jasonbauzon@gmail.com>
2022-07-28 01:01:12 -0500

    pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let member = parseInt(n);
+    if (member < 0) {
+        return "OOPS";
+    } else {
+        let array = [1, 1];
+        let nextNumber = 0;
+        for (let i = 1; i < member; i++) {
+            nextNumber = array[i] + array[i - 1];
+            array.push(nextNumber);
+        }
+        return array[member-1];
+    }
 };
 
 // Do not edit below this line
