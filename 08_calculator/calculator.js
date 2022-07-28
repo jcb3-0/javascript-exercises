247e2e3b9e721c9ecf9fad227ae3bfe1e4cad199
Jason Bauzon <jasonbauzon@gmail.com>
2022-07-28 01:04:48 -0500

    pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+	let sumOfAll = 0;
+  for (let arg of args) {
+    sumOfAll += arg;
+  }
+  return sumOfAll;
 };
 
-const multiply = function() {
-
+const multiply = function([...args]) {
+  let product = 1;
+  for (let arg of args) {
+    product *= arg;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans = 1;
+  for (let i = 1; i <= a; i++) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
