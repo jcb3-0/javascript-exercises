d008e07ef816485dafb26f66cc81cef9dda15a51
Jason Bauzon <jasonbauzon@gmail.com>
2022-07-30 00:06:08 -0500

    pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0].yearOfDeath - people[0].yearOfBirth;
+    for (let i = 1; i < people.length; i++) {
+        if (people[i].yearOfDeath - people[i].yearOfBirth > oldest) {
+            oldest = people[i];
+        }
+        return oldest;
+    }
 };
 
 // Do not edit below this line
