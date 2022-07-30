fcc5317ed137efb83b92af2666a1422c5f0ecf7b
Jason Bauzon <jasonbauzon@gmail.com>
2022-07-29 23:48:58 -0500

    pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(items) {
+    let titlesArray = [];
+    for (let item of items) {
+        titlesArray.push(item.title)
+    };
+    return titlesArray;
 };
 
 // Do not edit below this line
